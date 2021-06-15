efef71f3065bb51270de3f765f345650f824f1d4
Colin Szeto <kolinszeto@gmail.com>
2021-06-14 23:07:23 -0700

    added in varaible for ip address and port address

--- app.py
+++ app.py
@@ -22,7 +22,18 @@ def chat():
     if room:
         # return render_template('chat/chat.html', username=current_user.username, room=room)
         # return render_template('chat/chat.html', username=username, room=room)
-        return render_template('chat/chatAreaCss.html', username=username, room=room)
+        return render_template('chat/chatAreaCss.html', username=username, room=room, ip_address=ip_address, port_=port_ )
+    else:
+        return redirect('/chatIndex')
+
+@app.route("/chatOld")
+def chat_old():
+    room = request.args.get("room")
+    username = request.args.get("username")
+    if room:
+        # return render_template('chat/chat.html', username=current_user.username, room=room)
+        # return render_template('chat/chat.html', username=username, room=room)
+        return render_template('chat/chat.html', username=username, room=room, ip_address=ip_address, port_=port_ )
     else:
         return redirect('/chatIndex')
 
@@ -56,6 +67,8 @@ def handle_leave_room_event(data):
     leave_room(data['room'])
     socketio.emit('leave_room_announcement', data, room=data['room'])
 
+ip_address = "127.0.0.1"
+port_ = "3030"
 if __name__ == "__main__":
     # runs the application on the repl development server
-    socketio.run(app, debug=True, host='127.0.0.1', port='3030')
+    socketio.run(app, debug=True, host=ip_address, port=port_)
